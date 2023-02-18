66235eec34086fe85f900eced3c9ab585527815c
Hans Barraza <brain@Hanss-MacBook-Pro.local>
2023-02-18 13:03:54 -0700

    intial setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
